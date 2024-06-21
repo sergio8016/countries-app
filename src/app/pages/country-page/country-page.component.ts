e50423bb459c2fd48f6c53401bcbbec9162199d3
sergio <sergio8016@gmail.com>
2024-06-20 20:50:56 -0500

    showing country info by ID

--- src/app/pages/country-page/country-page.component.ts
+++ src/app/pages/country-page/country-page.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {map, switchMap, take} from "rxjs";
+import {CountriesService} from "../../services/countries.service";
+import {CountryInterface} from "../../interfaces/country.interface";
+import {DecimalPipe, KeyValuePipe, UpperCasePipe} from "@angular/common";
 
 @Component({
   selector: 'app-country-page',
   standalone: true,
-  imports: [],
+  imports: [
+    DecimalPipe,
+    KeyValuePipe,
+    UpperCasePipe
+  ],
   templateUrl: './country-page.component.html',
   styleUrl: './country-page.component.scss'
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit{
+  private activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+  private countriesService: CountriesService = inject(CountriesService);
+  private router: Router = inject(Router);
+  public country!: CountryInterface | null;
+  ngOnInit() {
+    this.activatedRoute.params
+      .pipe(
+        take(1),
+        switchMap( (params: Params) => this.countriesService.search(params['id'], 'alpha')),
+        map((countries: CountryInterface[]): CountryInterface | null => countries.length > 0 ? countries[0] : null)
+      )
+      .subscribe( (response: CountryInterface | null): Promise<boolean> | void => {
+        if (!response) return this.router.navigateByUrl('');
+        this.country = response;
+      });
+  }
+
 
 }
